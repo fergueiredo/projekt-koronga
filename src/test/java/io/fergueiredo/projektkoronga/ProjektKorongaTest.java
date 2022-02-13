616550d5cd53b9c5cea92e5acd18cee293ebe677
Fernando Alves de Figueiredo <fndaf90@gmail.com>
2022-02-13 22:47:09 +0000

    Create maven structure.

---
+++ src/test/java/io/fergueiredo/projektkoronga/ProjektKorongaTest.java
@@ -0,0 +1,14 @@
+package io.fergueiredo.projektkoronga;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+
+public class ProjektKorongaTest {
+
+    @Test
+    void shouldPass() {
+        assertThat(true).isTrue();
+    }
+}
