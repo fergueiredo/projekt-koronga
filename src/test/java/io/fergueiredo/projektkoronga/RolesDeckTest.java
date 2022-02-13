625a0696954a65dfde12fc310252871b2082d931
Fernando Alves de Figueiredo <fndaf90@gmail.com>
2022-02-13 23:06:08 +0000

    Add all missing roles.

--- src/test/java/io/fergueiredo/projektkoronga/RolesDeckTest.java
+++ src/test/java/io/fergueiredo/projektkoronga/RolesDeckTest.java
@@ -25,4 +25,49 @@ public class RolesDeckTest {
 
         assertThat(roles).contains(Role.QUARANTINE_EXPERT);
     }
+
+    @Test
+    void givenNewRolesDeck_whenGettingAvailableRoles_thenOperationExpertShouldExists() {
+        RolesDeck rolesDeck = new RolesDeck();
+
+        Set<Role> roles = rolesDeck.availableRoles();
+
+        assertThat(roles).contains(Role.OPERATION_EXPERT);
+    }
+
+    @Test
+    void givenNewRolesDeck_whenGettingAvailableRoles_thenScienceCommunicatorShouldExists() {
+        RolesDeck rolesDeck = new RolesDeck();
+
+        Set<Role> roles = rolesDeck.availableRoles();
+
+        assertThat(roles).contains(Role.SCIENCE_COMMUNICATOR);
+    }
+
+    @Test
+    void givenNewRolesDeck_whenGettingAvailableRoles_thenLogisticExpertShouldExists() {
+        RolesDeck rolesDeck = new RolesDeck();
+
+        Set<Role> roles = rolesDeck.availableRoles();
+
+        assertThat(roles).contains(Role.LOGISTIC_EXPERT);
+    }
+
+    @Test
+    void givenNewRolesDeck_whenGettingAvailableRoles_thenScientistShouldExists() {
+        RolesDeck rolesDeck = new RolesDeck();
+
+        Set<Role> roles = rolesDeck.availableRoles();
+
+        assertThat(roles).contains(Role.SCIENTIST);
+    }
+
+    @Test
+    void givenNewRolesDeck_whenGettingAvailableRoles_thenContingencyPlannerShouldExists() {
+        RolesDeck rolesDeck = new RolesDeck();
+
+        Set<Role> roles = rolesDeck.availableRoles();
+
+        assertThat(roles).contains(Role.CONTINGENCY_PLANNER);
+    }
 }
